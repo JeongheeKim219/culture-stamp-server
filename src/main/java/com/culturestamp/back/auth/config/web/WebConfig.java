9b08a43af6f1c55c55b3d04bfeac586eec2a3261
Jioo Jung <jioome17@gmail.com>
2023-03-28 21:33:57 +0900

    Update WebConfig.java

--- src/main/java/com/culturestamp/back/auth/config/web/WebConfig.java
+++ src/main/java/com/culturestamp/back/auth/config/web/WebConfig.java
@@ -12,8 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("*")
-            .allowedMethods("*")
+            .allowedOriginPatterns("http://localhost:3000","http://localhost:8080","https://localhost:3000",","https://localhost:8080",
+                "https://ec2-3-35-144-181.ap-northeast-2.compute.amazonaws.com", "https://ec2-3-35-144-181.ap-northeast-2.compute.amazonaws.com:8080")
+            .allowedMethods("GET", "POST", "PUT","DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(3000);
